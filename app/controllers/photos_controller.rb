cb0f68b19588d66a2adf85abba51d1eea3f05096
TaigaMikami <mikamimikami60@gmail.com>
2017-10-26 20:37:56 +0900

    photos_controller 追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,26 @@
 class PhotosController < ApplicationController
-  def new
+  def index
+    @photo = Photo.last
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(
+      boy_name: params[:boy_name],
+      boy_img_name: params[:boy_img_name],
+      girl_name: params[:girl_name],
+      girl_img_name: params[:girl_img_name]
+    )
+    if @photo.save
+      redirect_to("/photos/index")
+    else
+      render("/photos/new")
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-
-  get 'photos/new' => 'pthotos#new'
-  get
-
   get '/' => 'static_pages#top'
   get '/about' => 'static_pages#about'
+
+  get 'photos/index' => 'photos#index'
+  get 'photos/new' => 'photos#new'
+  post 'photos/create' => 'photos#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
